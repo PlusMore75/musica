a703365fabac8f931b5048915b762703783d035d
PlusMore75 <134657637+PlusMore75@users.noreply.github.com>
2026-03-10 10:36:26 -0500

    Update PurchasePage.jsx

--- front/src/PurchasePage.jsx
+++ front/src/PurchasePage.jsx
@@ -34,6 +34,7 @@ export default function PurchasePage({ selectedTracks = [], removeTrack }) {
       for (const track of selectedTracks) {
         await purchase({
           customer_id: Number(customerId),
+          track_id: Number(track.TrackId)
         });
       }
 
